f576c4beedcb1f87a18fc6a229fe9efc538b1858
lollobianco <loreb2002@gmail.com>
2022-11-10 14:32:02 +0100

    Aggiunta immagine da vue (bonus)

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,9 +1,10 @@
-var app = new Vue(
+var root = new Vue(
 	{
 	  el: '#root',
 		data:{
-         
-	    message: 'Ciao da Lorenzo Bianconi!!!',
+
+	    message: 'Ciao da uno scoiattolo!!!',
+       image: 'https://images.unsplash.com/photo-1568775071093-994349d340d4?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1674&q=80'
        
 	  }
 	}
